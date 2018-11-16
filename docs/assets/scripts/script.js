ecf48fcb8ca90e94dea0aa11450e26dbe0ddc24c
maple-momiji <32769079+maple-momiji@users.noreply.github.com>
2018-11-16 18:28:14 +0900

    [fix] 旧ブラウザ対応のため、jsの処理を変更

--- docs/assets/scripts/script.js
+++ docs/assets/scripts/script.js
@@ -1,14 +1,2 @@
-$('#btn').on('click', function(){
-  const array = shuffle([1,2,3,4]);
-  $('#area').text(array);
-});
-
-function shuffle(array) {
-  for (var i = array.length - 1; i >= 0; i--){
-    // 0~iのランダムな数値を取得
-    var rand = Math.floor( Math.random() * ( i + 1 ) );
-    // 配列の数値を入れ替える
-    [array[i], array[rand]] = [array[rand], array[i]]
-  }
-  return array;
-}
+$("#btn").on("click",function(){for(var a=[1,2,3,4],r=[],t=[],o=0;o<4;o++){var n=Math.floor(Math.random()*a.length);t=a.splice(n,n+1),Array.prototype.push.apply(r,t)}$("#area").text(r)});
+//# sourceMappingURL=script.js.map
